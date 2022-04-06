7a10cb6ce69b6b0159c51c189cab9f14e75ed7f7
Velicu Ana <92388303+anavelicu@users.noreply.github.com>
2022-04-06 16:01:32 +0300

    Add files via upload

---
+++ Gym Web Application/about.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>About</title>
+	<link rel="stylesheet" href="css/styleheader.css">
+	<link rel="stylesheet" href="css/styleabout.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@1,700&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+	  <?php
+  		include 'inc/header.php';
+  	?>
+	<div class = "content">
+		<div class = "container">
+			<div class = "left">
+				<div class = "row">
+					<div class = "col-md-6">
+						<h1>ABOUT US</h1>
+					</div>
+				</div>
+				<div class = "row">
+					<div class = "col-md-11">
+						<div class = "text">
+							<p style ="text-indent: 50px;">Hello there! This is Maria &amp; Ana and we created this webiste in order to inspire people to become more active. When we started creating this website we firstly put together what we want to see in a Fitness &amp; Wellnes wesite.</p>
+							<p style ="text-indent: 50px;">After a long brainstorming session we concluded that the interaction with the user is the most important. The user has to have a reason why he is accessing your website, right? That is why we created a personality/traits quiz which gives the user a full workout to try out.</p>
+							<p style ="text-indent: 50px;">We believe that food is a huge part of your fitness journey. So, we added a shopping list section whwre the user can build his own grocery list and take it with them!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class = "right">
+				<div class = "row">
+					<div class = "col-md-6">
+						<h3>You can find us here!</h3>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class = "map">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24854.77579902238!2d26.072168473122748!3d44.43333713167151!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b201ea132d5141%3A0xf5d5aecb94fa6dc7!2sUniversity%20POLITEHNICA%20of%20Bucharest!5e0!3m2!1sen!2sro!4v1589728580813!5m2!1sen!2sro" width="450" height="430" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+		
+
+
+</body>
+</html>
